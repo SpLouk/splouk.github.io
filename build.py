8ebe2f9e2d7de3d02f3540c1037c0c9025a6d8a2
David Loukidelis <david@loukidelis.com>
2022-11-27 13:40:15 -0500

    basic build script

---
+++ build.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+import shutil
+import os
+import json
+import chevron
+
+
+def generate_articles():
+    articles = []
+    for root, dirs, files in os.walk("./src"):
+        if "article.json" in files:
+            metadata = generate_article_get_metadata(root)
+            articles.append(metadata)
+    generate_index(articles)
+
+def generate_index(articles):
+    with open(f"./src/index.html.mustache", "r") as f:
+        article_content = f.read()
+    with open("./src/article-template.html.mustache", "r") as f:
+        html_output = chevron.render(**{
+            'template': f,
+            'data': {
+                "show_title": False,
+                "articles": articles
+            },
+            'partials_dict': {
+                "article_content": article_content,
+            },
+        })
+    with open(f"./build/index.html", "w") as output_file:
+        output_file.write(html_output)
+
+# Generate an article from the given dir, return the metadata
+def generate_article_get_metadata(root: str):
+    with open(f"{root}/article.json", "r") as f:
+        metadata = json.load(f)
+    with open(f"{root}/content.html", "r") as f:
+        article_content = f.read()
+    with open("./src/article-template.html.mustache", "r") as f:
+        html_output = chevron.render(**{
+            'template': f,
+            'data': {
+                "title": metadata["title"],
+                "show_title": ("title" in metadata),
+                "date": metadata["date"],
+            },
+            'partials_dict': {
+                "article_content": article_content,
+            },
+        })
+
+    # Write the file
+    with open(f"./build/{metadata['url']}.html", "w") as output_file:
+        output_file.write(html_output)
+    
+    # copy over resources
+    for _, dirs, files in os.walk(f"{root}/resources"):
+        for file in files:
+            shutil.copy(f"{root}/resources/{file}", "./build/resources/")
+
+    return metadata
+
+
+if __name__ == "__main__":
+    os.makedirs('build/resources', exist_ok=True)
+    generate_articles()
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start tag:", tag)
+        for attr in attrs:
+            print("     attr:", attr)
+
+    def handle_endtag(self, tag):
+        print("End tag  :", tag)
+
+    def handle_data(self, data):
+        print("Data     :", data)
+
+    def handle_comment(self, data):
+        print("Comment  :", data)
+
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+
+    def handle_charref(self, name):
+        if name.startswith("x"):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+
+    def handle_decl(self, data):
+        print("Decl     :", data)
+
+
+parser = MyHTMLParser()
